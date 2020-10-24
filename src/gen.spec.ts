8000c05464910e194789ef3f50a8bcceddd800ca
Susisu <susisu2413@yahoo.co.jp>
2020-10-24 20:43:38 +0900

    test: remove trivial tests and improve them

--- src/gen.spec.ts
+++ src/gen.spec.ts
@@ -1,4 +1,5 @@
 import { TFGen } from "./gen";
+import { Int32x8 } from "./tf";
 
 function initGen(): TFGen {
   return TFGen.seed(
@@ -16,7 +17,7 @@ function initGen(): TFGen {
 describe("TFGen", () => {
   describe(".seed", () => {
     it("should initialize a generator", () => {
-      const gen = TFGen.seed(
+      const seed: Int32x8 = [
         0x00000000,
         0x00000000,
         0x01234567,
@@ -24,48 +25,56 @@ describe("TFGen", () => {
         0x89abcdef,
         0x01234567,
         0xffffffff,
-        0xffffffff
-      );
-      expect(gen).toBeInstanceOf(TFGen);
+        0xffffffff,
+      ];
+      const gen1 = TFGen.seed(...seed);
+      const gen2 = TFGen.seed(...seed);
+      const [val1] = gen1.next();
+      const [val2] = gen2.next();
+      expect(val1).toBe(val2);
     });
   });
 
   describe(".init", () => {
     it("should initialize a generator using randomly generated seeds", () => {
-      const gen = TFGen.init();
-      expect(gen).toBeInstanceOf(TFGen);
+      const gen1 = TFGen.init();
+      const gen2 = TFGen.init();
+      const [val1] = gen1.next();
+      const [val2] = gen2.next();
+      expect(val1).not.toBe(val2);
     });
   });
 
   describe("#next", () => {
     it("should return a pair of random 32-bit integer and a new generator", () => {
       const gen = initGen();
-      const p = gen.next();
-      expect(p).toHaveLength(2);
-      const [val, newGen] = p;
+      const [val, nextGen] = gen.next();
       expect(val).toMatchInlineSnapshot(`78121409`);
       expect(val).toBeGreaterThanOrEqual(-0x80000000);
       expect(val).toBeLessThanOrEqual(0x7fffffff);
-      expect(newGen).toBeInstanceOf(TFGen);
+      expect(nextGen).not.toBe(gen);
     });
   });
 
   describe("#split", () => {
     it("should return a pair of new generators", () => {
       const gen = initGen();
-      const p = gen.split();
-      expect(p).toHaveLength(2);
-      const [left, right] = p;
-      expect(left).toBeInstanceOf(TFGen);
-      expect(right).toBeInstanceOf(TFGen);
+      const [left, right] = gen.split();
+      expect(left).not.toBe(gen);
+      expect(right).not.toBe(gen);
+      expect(left).not.toBe(right);
     });
   });
 
   describe("#level", () => {
-    it("should return a new generator", () => {
-      const gen = initGen();
-      const newGen = gen.level();
-      expect(newGen).toBeInstanceOf(TFGen);
+    it("should return a new generator if needed", () => {
+      const gen1 = initGen();
+      const newGen1 = gen1.level();
+      expect(newGen1).toBe(gen1);
+
+      const gen2 = gen1.splitn(32, 0);
+      const newGen2 = gen2.level();
+      expect(newGen2).not.toBe(gen2);
     });
   });
 
@@ -73,7 +82,7 @@ describe("TFGen", () => {
     it("should return a new generator", () => {
       const gen = initGen();
       const newGen = gen.splitn(32, 0x7fffffff);
-      expect(newGen).toBeInstanceOf(TFGen);
+      expect(newGen).not.toBe(gen);
     });
   });
 });
--- src/random.spec.ts
+++ src/random.spec.ts
@@ -24,24 +24,24 @@ function initGen(): TFGen {
 }
 
 describe("randomInt32", () => {
-  it("should generate a random 32-bit signed integer and a new generator", () => {
+  it("should generate a random 32-bit signed integer and a next generator", () => {
     const gen = initGen();
     const [val, nextGen] = randomInt32(gen);
     expect(val).toMatchInlineSnapshot(`78121409`);
     expect(val).toBeGreaterThanOrEqual(-0x80000000);
     expect(val).toBeLessThanOrEqual(0x7fffffff);
-    expect(nextGen).toBeInstanceOf(TFGen);
+    expect(nextGen).not.toBe(gen);
   });
 });
 
 describe("randomInt32R", () => {
-  it("should generate a random 32-bit signed integer within a bounds and a new generator", () => {
+  it("should generate a random 32-bit signed integer within a bounds and a next generator", () => {
     const gen = initGen();
     const [val, nextGen] = randomInt32R(gen, [-0x8000, 0x7fff]);
     expect(val).toMatchInlineSnapshot(`-30271`);
     expect(val).toBeGreaterThanOrEqual(-0x8000);
     expect(val).toBeLessThanOrEqual(0x7fff);
-    expect(nextGen).toBeInstanceOf(TFGen);
+    expect(nextGen).not.toBe(gen);
   });
 
   it("should work if bounds are flipped", () => {
@@ -50,36 +50,36 @@ describe("randomInt32R", () => {
     expect(val).toMatchInlineSnapshot(`-30271`);
     expect(val).toBeGreaterThanOrEqual(-0x8000);
     expect(val).toBeLessThanOrEqual(0x7fff);
-    expect(nextGen).toBeInstanceOf(TFGen);
+    expect(nextGen).not.toBe(gen);
   });
 
   it("should work if bounds are equal", () => {
     const gen = initGen();
     const [val, nextGen] = randomInt32R(gen, [0x7fff, 0x7fff]);
     expect(val).toBe(0x7fff);
-    expect(nextGen).toBeInstanceOf(TFGen);
+    expect(nextGen).toBe(gen);
   });
 });
 
 describe("randomUint32", () => {
-  it("should generate a random 32-bit unsigned integer and a new generator", () => {
+  it("should generate a random 32-bit unsigned integer and a next generator", () => {
     const gen = initGen();
     const [val, nextGen] = randomUint32(gen);
     expect(val).toMatchInlineSnapshot(`78121409`);
     expect(val).toBeGreaterThanOrEqual(0x00000000);
     expect(val).toBeLessThanOrEqual(0xffffffff);
-    expect(nextGen).toBeInstanceOf(TFGen);
+    expect(nextGen).not.toBe(gen);
   });
 });
 
 describe("randomUint32R", () => {
-  it("should generate a random 32-bit unsigned integer within a bounds and a new generator", () => {
+  it("should generate a random 32-bit unsigned integer within a bounds and a next generator", () => {
     const gen = initGen();
     const [val, nextGen] = randomUint32R(gen, [0xffff0000, 0xffffffff]);
     expect(val).toMatchInlineSnapshot(`4294904257`);
     expect(val).toBeGreaterThanOrEqual(0xffff0000);
     expect(val).toBeLessThanOrEqual(0xffffffff);
-    expect(nextGen).toBeInstanceOf(TFGen);
+    expect(nextGen).not.toBe(gen);
   });
 
   it("should work if bounds are flipped", () => {
@@ -88,71 +88,70 @@ describe("randomUint32R", () => {
     expect(val).toMatchInlineSnapshot(`4294904257`);
     expect(val).toBeGreaterThanOrEqual(0xffff0000);
     expect(val).toBeLessThanOrEqual(0xffffffff);
-    expect(nextGen).toBeInstanceOf(TFGen);
+    expect(nextGen).not.toBe(gen);
   });
 
   it("should work if bounds are equal", () => {
     const gen = initGen();
     const [val, nextGen] = randomUint32R(gen, [0xffff0000, 0xffff0000]);
     expect(val).toBe(0xffff0000);
-    expect(nextGen).toBeInstanceOf(TFGen);
+    expect(nextGen).toBe(gen);
   });
 });
 
 describe("randomBoolean", () => {
-  it("should generate a random boolean value and a new generator", () => {
+  it("should generate a random boolean value and a next generator", () => {
     const gen = initGen();
     const [val, nextGen] = randomBoolean(gen);
     expect(val).toMatchInlineSnapshot(`false`);
-    expect(typeof val).toBe("boolean");
-    expect(nextGen).toBeInstanceOf(TFGen);
+    expect(nextGen).not.toBe(gen);
   });
 });
 
 describe("randomInt", () => {
-  it("should generate a random safe integer and a new generator", () => {
+  it("should generate a random safe integer and a next generator", () => {
     const gen = initGen();
     const [val, nextGen] = randomInt(gen);
     expect(val).toMatchInlineSnapshot(`-4543985126733374`);
     expect(Number.isSafeInteger(val)).toBe(true);
-    expect(nextGen).toBeInstanceOf(TFGen);
+    expect(nextGen).not.toBe(gen);
   });
 });
 
 describe("randomIntR", () => {
-  it("should generate a random safe integer within a bounds and a new generator", () => {
+  it("should generate a random safe integer within a bounds and a next generator", () => {
     const gen = initGen();
     const [val, nextGen] = randomIntR(gen, [-100, 100]);
     expect(val).toMatchInlineSnapshot(`93`);
     expect(val).toBeGreaterThanOrEqual(-100);
     expect(val).toBeLessThanOrEqual(100);
-    expect(nextGen).toBeInstanceOf(TFGen);
+    expect(nextGen).not.toBe(gen);
   });
 
-  it("should be OK if bounds are flipped", () => {
+  it("should work if bounds are flipped", () => {
     const gen = initGen();
     const [val, nextGen] = randomIntR(gen, [100, -100]);
     expect(val).toMatchInlineSnapshot(`93`);
     expect(val).toBeGreaterThanOrEqual(-100);
     expect(val).toBeLessThanOrEqual(100);
-    expect(nextGen).toBeInstanceOf(TFGen);
+    expect(nextGen).not.toBe(gen);
   });
 
-  it("should be OK if bounds are equal", () => {
+  it("should work if bounds are equal", () => {
     const gen = initGen();
     const [val, nextGen] = randomIntR(gen, [42, 42]);
     expect(val).toBe(42);
-    expect(nextGen).toBeInstanceOf(TFGen);
+    expect(nextGen).toBe(gen);
   });
 });
 
 describe("random", () => {
-  it("should generate a random number within [0, 1) and a new generator", () => {
+  it("should generate a random number within [0, 1) and a next generator", () => {
     const gen = initGen();
     const [val, nextGen] = random(gen);
     expect(val).toMatchInlineSnapshot(`0.49551630887463477`);
     expect(val).toBeGreaterThanOrEqual(0);
     expect(val).toBeLessThan(1);
-    expect(nextGen).toBeInstanceOf(TFGen);
+    expect(nextGen).not.toBe(gen);
   });
 });
